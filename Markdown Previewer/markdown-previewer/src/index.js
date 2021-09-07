171f619157f72a1fe31bf614e9eaa4610b9aa8ae
CLanham01 <c.lanham27@gmail.com>
2021-09-07 11:10:30 -0500

    update minus parser and css

--- Markdown Previewer/markdown-previewer/src/index.js
+++ Markdown Previewer/markdown-previewer/src/index.js
@@ -1,12 +1,64 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
 import reportWebVitals from './reportWebVitals';
+
+const placeholder = "This is still a test"
+
+class MarkdownPreviewer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      markdown: placeholder
+    };
+
+    this.handlechange = this.handlechange.bind(this)
+
+  }
+
+
+  handlechange(e) {
+    this.setState({
+      markdown: e.target.value
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Markdown Previwer</h1>
+        <Editor markdown={this.state.markdown} onChange={this.handlechange} />
+        <Viewer markdown={this.state.markdown} />
+      </div>
+    );
+  }
+};
+
+const Editor = (props) => {
+  return (
+    <div>
+      <h2>Editor</h2>
+      <h3>Enter your Markdown here</h3>
+      <textarea onChange={props.onChange} type='text' value={props.markdown} />
+    </div>
+  );
+}
+
+const Viewer = (props) => {
+  return (
+    <div>
+      <h2>Viewer</h2>
+      <h3>See you markdown here</h3>
+      <div>{props.markdown}</div>
+    </div>
+  )
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+
+    <MarkdownPreviewer />
   </React.StrictMode>,
   document.getElementById('root')
 );
